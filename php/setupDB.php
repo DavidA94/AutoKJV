31b24239de080c894e8ed98ae35df3c8ef3260c3
David A <David A>
2015-12-25 15:48:18 -0800

    Fixing missing slash

--- php/setupDB.php
+++ php/setupDB.php
@@ -6,7 +6,7 @@ require_once('databaseVars.php');
 $db = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$pass");
 
 // Get the SQL KJV DB we want to insert
-$sql = file_get_contents(dirname(__FILE__) . 'sql/bibledb_kjv.sql');
+$sql = file_get_contents(dirname(__FILE__) . '/sql/bibledb_kjv.sql');
 
 // Execute it.
 $qr = $db->exec($sql);
